ae5255b9685f0726b3d30b845f48244cd142fe23
jagadishreddy31 <jagadiswara_nallamilli@srmap.edu.in>
2026-02-11 11:07:43 +0530

    Add files via upload

---
+++ day 3 [student performance analyzer].py
@@ -0,0 +1,49 @@
+N = int(input("Enter number of students: "))
+
+names = [""] * N
+marks = [0] * N
+
+for i in range(N):
+    names[i] = str(input(f"Enter name of Student {i+1} :"))
+    marks[i] = int(input(f"Enter mark of Student {i+1}:"))
+
+valid_count = 0
+fail_count = 0
+
+
+
+for i in range(N):
+
+    name = names[i]
+    mark = marks[i]
+
+    if mark < 0 or mark > 100:
+        print(mark, "→ Invalid")
+
+    else:
+        if len(name) % 2 == 0:
+            mark = mark + 5
+            if mark > 100:
+                mark = 100
+
+        valid_count = valid_count + 1
+
+        if mark >= 90:
+            print(mark, "→ Excellent")
+
+        elif mark >= 75:
+            print(mark, "→ Very Good")
+
+        elif mark >= 60:
+            print(mark, "→ Good")
+
+        elif mark >= 40:
+            print(mark, "→ Average")
+
+        else:
+            print(mark, "→ Fail")
+            fail_count = fail_count + 1
+
+
+print("Total Valid Students:", valid_count)
+print("Total Failed Students:", fail_count)
